837e4f11cbd8461786204b92d6c0cfe1b923104b
Alan Jean <afinegameofnil@gmail.com>
2023-02-02 03:45:05 +0400

    feat: add ability to export data to csv/xlsx from datatables

--- src/js/dataTable.js
+++ src/js/dataTable.js
@@ -1,14 +1,41 @@
+import $ from 'jquery';
+import * as jszip from 'jszip';
 import DataTable from 'datatables.net-bs5';
 import 'datatables.net-buttons-bs5';
 import 'datatables.net-buttons/js/buttons.html5.mjs';
-import DateTime from 'datatables.net-datetime';
 import 'datatables.net-searchbuilder-bs5';
 import 'datatables.net-searchpanes-bs5';
+
+
+$.fn.dataTable.Buttons.jszip(jszip);
 
 
 export const buildDataTable = (selector, opts) => new DataTable(
   selector, {
     responsive: true,
+    ordering: true,
+    buttons: [
+      { extend: 'csvHtml5',   className: 'mr-2' },
+      { extend: 'excelHtml5', className: 'ml-2 btn-info' },
+    ],
+    dom: `
+      <'d-flex flex-row bd-highlight mb-2 align-items-center'
+        <'mb-2 bd-highlight ms-auto' >
+        <'export-btn-label mx-2'>
+        B
+      >
+      <'row'
+        <'col-sm-6 col-md-6'l>
+        <'col-sm-12 col-md-6'f>
+      >
+      <'row'
+        <'col-sm-12'tr>
+      >
+      <'row'
+        <'col-sm-12 col-md-5'i>
+        <'col-sm-12 col-md-7'p>
+      >
+    `,
     ...opts
   })
 ;
